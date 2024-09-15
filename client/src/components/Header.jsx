263e5fe4007c5d65e01eb6997fb16628ee5b9495
Shashank <shankga3112@gmail.com>
2024-09-15 09:38:56 +0530

    Add dark mode functionalidy and header data update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,14 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const { theme }  = useSelector((state) => state.theme);
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -32,15 +37,39 @@ export default function Header() {
           className='w-12 h-10 hidden sm:inline'
           color='gray'
           pill
+          onClick={() => dispatch(toggleTheme())}
         >
-         <FaMoon />
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+           
         </Button>
         
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
           <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
+        )}
     
         <Navbar.Toggle />
       </div>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import { persistReducer, persistStore } from 'redux-persist';
+import themeReducer from './theme/themeSlice'
 import storage from 'redux-persist/lib/storage';
+import { theme } from 'flowbite-react';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
