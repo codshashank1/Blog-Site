a4fe25f004e108a621b116876b4d964563effbc8
Shashank <shankga3112@gmail.com>
2024-09-27 11:45:23 +0530

    add posts section to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -13,11 +13,15 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
 
+
+
 export default function DashSidebar() {
   const location = useLocation();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState('');
+ 
+
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -48,8 +52,8 @@ export default function DashSidebar() {
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
-              icon={HiUser}
-              label={'User'}
+              icon={HiUser}  
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
               labelColor={'dark'}
               as='div'
 
@@ -57,6 +61,17 @@ export default function DashSidebar() {
             Profile
             </Sidebar.Item>
           </Link> 
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmRight}
             className='cursor-pointer'
