8d2b468ea4d8c64cc405d6d886ae2376db283320
Shashank <shankga3112@gmail.com>
2024-09-28 12:47:37 +0530

    add comment section to post page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGO)
@@ -23,6 +24,7 @@ app.use(cookieParser())
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
